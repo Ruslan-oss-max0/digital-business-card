a6c5869d703b48d3723f00f4f2cd6590ba1cc358
Ruslan <kryzhanskiyruslan@gmail.com>
2025-07-22 12:03:00 +0300

    impement the Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.scss";
 import { Info } from "./components/Info/Info";
 import { About } from "./components/About/About";
+import { Footer } from "./components/Footer/Footer";
 
 export const App = () => {
   return (
     <>
       <Info />
       <About />
+      <Footer />
     </>
   );
 };
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import './Footer.scss';
+
+export const Footer = () => {
+  return (
+    <footer className='footer'>
+      <div className='footer__icons'>
+        <a className='icon twitter-icon' href="#"></a>
+        <a className='icon facebook-icon' href="#"></a>
+        <a className='icon instagram-icon' href="#"></a>
+        <a className='icon github-icon' href="#"></a>
+      </div>
+    </footer>
+  )
+}
